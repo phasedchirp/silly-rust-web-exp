aea6e0abdebecf81565f89a7f0d9350a0e0d81a0
phasedchirp <phasedchirp2718@gmail.com>
2017-01-05 22:13:37 -0800

    some reorganization

--- src/main.rs
+++ src/main.rs
@@ -15,45 +15,12 @@ use std::fs::{File,remove_file,read_dir};
 use std::io::{Read,Write};
 use std::sync::{Arc,RwLock,Mutex};
 
-mod make_id;
-use make_id::new_id;
+mod utils;
+use utils::*;
 
 
-#[derive(RustcEncodable,Clone,Debug)]
-struct Question {
-    number: usize,
-    text: String,
-    options: Option<Vec<String>>
-}
-
-#[derive(RustcEncodable,Clone,Debug)]
-struct Survey {
-    path: String,
-    questions: Vec<Question>
-}
-
-#[derive(Debug)]
-struct SResponse {
-    id: String,
-    vals: Vec<(String,String)>
-}
-
-fn make_questions(qs: &Vec<&str>) -> Vec<Question> {
-    let mut result = Vec::new();
-    for (i,q) in qs.iter().enumerate() {
-        let mut q_opts = q.trim().split(':').collect::<Vec<&str>>();
-        println!("{:?}", q_opts);
-        let opts : Option<Vec<String>> = match q_opts.len() > 1 {
-            true => Some(q_opts[1].split(',').map(|s| s.to_string()).collect()),
-            false => None
-        };
-        result.push(Question{number:i,text:q_opts[0].to_string(),options:opts});
-    }
-    result
-}
 
 fn survey_from_file(survey_file: &str) -> Result<Vec<Question>,u32> {
-    // let survey_file = format!("surveys/{}",id);
     match File::open(survey_file) {
         Ok(mut f) => {
             let mut buf = String::new();
@@ -65,53 +32,10 @@ fn survey_from_file(survey_file: &str) -> Result<Vec<Question>,u32> {
     }
 }
 
-fn parse_survey(s: &Vec<Question>) -> String {
-    let mut result = String::new();
-    for q in s {
-        let current_q = match q.options {
-            None => format!("{t}<br><input type=\"text\" name=\"q{n}\"></br>",t = q.text, n = q.number),
-            Some(ref opts) => {
-                let mut temp = format!("{t}<br>",t=q.text);
-                for opt in opts {
-                    temp.push_str(&format!("<input type=\"radio\" name=\"q{n}\" value=\"{o}\">{o}<br>",n=q.number, o=opt));
-                }
-                temp
-            }
-        };
-        result.push_str(&current_q);
-    }
-    result
-}
 
-fn prep_resp_statement(resp: &SResponse, id: &str, t: &str) -> String {
-    let mut stmnt = "INSERT INTO responses (id,".to_string();
-    let mut vals = format!("VALUES ({}",id);
-    for r in &resp.vals {
-        stmnt.push_str(&format!("q{},", r.0));
-        vals.push_str(&format!("{},",r.1));
-    }
-    stmnt.push_str("time)");
-    stmnt.push_str(&format!("{})",t));
-    stmnt.push_str(&vals);
-    stmnt
-}
-
-fn prep_insert_statement(s: &Survey) -> String {
-    let mut stmnt = "CREATE TABLE responses (id string PRIMARY KEY,".to_string();
-    for q in 0..(s.questions.len()) {
-        stmnt.push_str(&format!("q{} TEXT,\n",q));
-    }
-    stmnt.push_str("time string\n)");
-    stmnt
-}
 
 fn main() {
     let mut server = Nickel::new();
-
-    // currently creating one large db for everything
-    // could also create db for each survey instead
-    // relevant docs:
-    // http://jgallagher.github.io/rusqlite/rusqlite/struct.Connection.html
     let mut conn_arc = Arc::new(Mutex::new(Connection::open_in_memory().unwrap()));
 
     let mut surveys = HashMap::new();
@@ -124,12 +48,9 @@ fn main() {
         }
     }
 
-    // See following example for approach to sharing data between handlers:
-    // https://github.com/nickel-org/nickel.rs/blob/master/examples/route_data.rs
     let mut surveys_arc = Arc::new(RwLock::new(surveys));
 
-    //middleware function logs each request to console
-    // taken from https://github.com/Codenator81/nickel-demo
+    // log requests to stdout:
     server.utilize(middleware! { |request|
         println!("logging request: {:?}", request.origin.uri);
     });
